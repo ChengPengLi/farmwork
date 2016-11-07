c91be3fd6ff51c4b5aab93623c1fe082da491cf5
LiPengcheng <lipengcheng@ky3h.com>
2016-11-07 09:40:06 +0800

    111

---
+++ farmwork/app/src/main/java/com/ky3h/farmwork/SplashActivity.java
@@ -0,0 +1,8 @@
+package com.ky3h.farmwork;
+
+/**
+ * Created by lipengcheng on 2016/9/1.
+ * you are sb
+ */
+public class SplashActivity {
+}
---
+++ farmwork/app/src/main/java/com/ky3h/farmwork/widget/ConstemView.java
@@ -0,0 +1,8 @@
+package com.ky3h.farmwork.widget;
+
+/**
+ * Created by lipengcheng on 2016/9/13.
+ * you are sb
+ */
+public class ConstemView {
+}
